da714029a689a3c3b62575131284e9fb7a4f70da
avianflame <theavianflame@gmail.com>
2024-12-31 13:04:04 -0600

    added a basic react component

---
+++ src/components/Tool.jsx
@@ -0,0 +1,8 @@
+export default function Tool () {
+    return (
+        <div>
+            <img src="https://blog.omlet.co.uk/wp-content/uploads/sites/9/2020/01/Pet-Care-Advice-Rabbit2-1-1-1024x768-1024x768.jpg" alt="babbit" />
+            <p>i ate all the tools</p>
+        </div>
+    );
+}
